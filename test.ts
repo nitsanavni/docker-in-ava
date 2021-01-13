c43fc2609d4c9d55fa77dc79febe64477b51c8f5
Nitsan Avni <nitsan@papayaglobal.com>
2021-01-13 09:57:53 +0200

    less ambiguous dates

--- test.ts
+++ test.ts
@@ -4,14 +4,10 @@ import { DateTime } from "luxon";
 
 test("docker run -> ubuntu iso date", async (t) => {
     const { stdout: actualOuput } = await command(
-        "docker run --rm ubuntu date -d next\\ Friday -I"
+        "docker run --rm ubuntu date -I"
     );
 
-    const expectedOuput = DateTime.local()
-        // next Friday
-        .plus({ week: 1 })
-        .set({ weekday: 5 })
-        .toISODate();
+    const expectedOuput = DateTime.local().toISODate();
 
     t.is(actualOuput, expectedOuput);
 });
